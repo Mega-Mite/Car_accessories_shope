eeb0d37c14fdfc7f5cae5862640434d530063663
Mr.Meg <megalodonfatalbite@gmail.com>
2025-11-04 12:19:21 +0530

    Update vite.config.js
    vite_updatin default port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- server: {
+  server: {
     host: '0.0.0.0',
+    port: process.env.PORT || 5173
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  }
 })
